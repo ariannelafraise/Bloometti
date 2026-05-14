ea37a689050d1f92a7c8670d269f4c026b7ca86f
Mizari-Dev <warriorofmeadow@gmail.com>
2026-05-14 18:40:34 +0200

    fix: https://github.com/ariannelafraise/bloometti/pull/2#discussion_r3238078886

--- src/control/LeaderboardService.js
+++ src/control/LeaderboardService.js
@@ -22,7 +22,9 @@ class LeaderboardService {
   }
 
   async generateBoard(users) {
+    // Sort users
     users.sort((a,b)=>a.chatting.level === b.chatting.level?b.chatting.exp-a.chatting.exp:b.chatting.level-a.chatting.level);
+    // Get 10 firsts
     if (users.length > 10){
       users = users.slice(0, 10);
     }
@@ -34,46 +36,67 @@ class LeaderboardService {
     context.fillRect(0, 0, canvas.width, canvas.height);
     // Text and lines
     const textColor = "#cdd6f4";
-    context.font = "24px";
-    context.fillStyle = textColor;
-    context.fillText("Rank", 3, 25);
-    context.fillText("Username", 100, 25);
-    context.fillText("Level", 500, 25);
-    context.fillText("EXP", 600, 25);
-
-    let height;
-    let bar;
-    let lineGrad;
-    for (const user of users) {
-      const index = users.indexOf(user);
-      height = 53+31*index;
-      bar = await this.#profileService.generateProgressBar(user, 85, 10);
-      bar = Buffer.from(bar.attachment,"binary").toString("base64");
-      bar = await Canvas.loadImage("data:image/png;base64,"+bar);
-      lineGrad = context.createLinearGradient(0, height-26, 698, height+15);
-      lineGrad.addColorStop(0, user.color+ "A9");
-      lineGrad.addColorStop(1, "transparent");
-      context.fillStyle = lineGrad ;
-      context.fillRect(2, height-24, 698, 31);
-      context.fillStyle = textColor;
-      context.fillText(index+1, 0, height);
-      context.fillText(user.username, 102, height);
-      context.fillText(user.chatting.level, 502, height);
-      //context.fillText(user.chatting.exp, 602, height);
-      context.drawImage(bar, 602, height-bar.height, bar.width, bar.height);
-    }
-    context.fillRect(0, 0, 2, 1000);
-    context.fillRect(0, 0, 2, 1000);
-    context.fillRect(90, 0, 2, 1000);
-    context.fillRect(490, 0, 2, 1000);
-    context.fillRect(590, 0, 2, 1000);
-    context.fillRect(699, 0, 2, 1000);
-    context.fillRect(0, 28, 700, 1);
-    
+    this.drawTable(context, textColor, canvas.width, canvas.height);
+    // Raws
+    await this.drawRaws(context, textColor, users);
     // Export the progress bar image as png
     return new AttachmentBuilder(await canvas.toBuffer(), {
         name: "Test.png",
     });
+  }
+
+  drawTable(ctx, color, width, height){
+    ctx.font = "24px";
+    ctx.fillStyle = color;
+    // Tiltes
+    ctx.fillText("Rank", 10, 25);
+    ctx.fillText("Username", 100, 25);
+    ctx.fillText("Level", 500, 25);
+    ctx.fillText("EXP", 600, 25);
+    // Lines
+    ctx.fillRect(0, 0, 2, height);
+    ctx.fillRect(90, 0, 2, height);
+    ctx.fillRect(490, 0, 2, height);
+    ctx.fillRect(590, 0, 2, height);
+    ctx.fillRect(698, 0, 2, height);
+    ctx.fillRect(0, 28, width, 2);
+  }
+
+  async drawRaws(ctx, txtColor, usrs){
+    // Init variables (optimization)
+    let usr;
+    let height;
+    let lineGrad;
+    let bar;
+    // For all users
+    for (let index in usrs) {
+      index = parseInt(index);
+      usr = usrs[index];
+      height = 53+31*index;
+      // Draw raw background as a gradient
+      lineGrad = ctx.createLinearGradient(0, height-26, 698, height+15);
+      lineGrad.addColorStop(0, usr.color+ "A9");
+      lineGrad.addColorStop(1, "transparent");
+      ctx.fillStyle = lineGrad ;
+      ctx.fillRect(2, height-24, 698, 31);
+      // Write user informations
+      ctx.fillStyle = txtColor;
+      ctx.fillText(index+1, 10, height); // Rank
+      ctx.fillText(usr.username, 100, height); // Username
+      ctx.fillText(usr.chatting.level, 500, height); // Level
+      // Draw progress bar
+      bar = await this.getUserProgressBar(usr);
+      ctx.drawImage(bar, 600, height-(bar.height+2), bar.width, bar.height);
+    }
+  }
+
+  async getUserProgressBar(usr){
+    // Get user progress bar as a buffer
+    const progressBar = await this.#profileService.generateProgressBar(usr, 88, 10);
+    // Convert into base64
+    const pbBase64 = Buffer.from(progressBar.attachment,"binary").toString("base64");
+    // Load as canvas image
+    return await Canvas.loadImage("data:image/png;base64,"+pbBase64);
   }
 }
 
